bba4533d45fa4f4b784e94ae51db96f18e815268
Manoj Kumar R <manutvm07@gmail.com>
2019-06-17 23:51:15 +0530

    Test Jenkinsfile

--- Modules/NetworkSecurityGroup/main.tf
+++ Modules/NetworkSecurityGroup/main.tf
@@ -22,3 +22,18 @@ resource "azurerm_network_security_rule" "networksecurityrole" {
   network_security_group_name = "${azurerm_network_security_group.networksg.name}"
   resource_group_name         = "${azurerm_network_security_group.networksg.resource_group_name}"
 }
+
+resource "azurerm_network_security_rule" "networksecurityrole" {
+  name                        = "${var.name}NetworkSecurityGroup_Port6443"
+  priority                    = 101
+  direction                   = "Outbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_address_prefix       = "*"
+  source_port_range           = "*"
+  destination_address_prefix  = "VirtualNetwork"
+  destination_port_range      = "6443"
+  description                 = "Port 6443 for Kubernetes Configuration"
+  network_security_group_name = "${azurerm_network_security_group.networksg.name}"
+  resource_group_name         = "${azurerm_network_security_group.networksg.resource_group_name}"
+}
